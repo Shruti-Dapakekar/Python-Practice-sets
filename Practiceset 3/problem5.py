032b8e253dda924b199a956f4a6f66579f0c3f42
Shruti-Dapakekar <shrutidapakekar@gmail.com>
2026-09-01 18:36:26 +0530

    modified problem5.py

--- Practiceset 3/problem5.py
+++ Practiceset 3/problem5.py
@@ -1,5 +1,5 @@
 #WAP to formate the following letter using escape sequence characters
 letter = "Dear Shruti,this python course is nice. Thanks!"
 print("Origibnal sentence is: ",letter)
-letterf=("\nDear \"Shruti\",\nthis python course is nice. \nThanks!")
+letterf=("\nDear \"Shruti\",\n\tThis python course is nice. \nThanks!")
 print("Formated sentence is: ",letterf)
