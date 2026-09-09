02c1b426cf89c80cad0818668b5db4a9c0e67546
Shruti-Dapakekar <shrutidapakekar@gmail.com>
2026-09-09 16:14:55 +0530

    Practice set 4 adding...

---
+++ Practiceset 4/problem1.py
@@ -0,0 +1,8 @@
+# friends=['Alice', 'Bob', 'Charlie', 'David', 'Eve',7,False]
+# print(friends)
+# print(friends[0])
+# friends[0]='Zoe'
+# print(friends[1:6])
+a=(1,2,3,4,5,1,7,1)
+print(a.count(1))
+print(a.index(1))
