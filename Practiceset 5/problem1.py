7b165657d27e2d86a35d8347bff020824bb50250
Shruti-Dapakekar <shrutidapakekar@gmail.com>
2026-09-11 23:04:06 +0530

    solved problem 1 & 2

---
+++ Practiceset 5/problem1.py
@@ -0,0 +1,7 @@
+#Write a program to create a dictionary of Hindi words with values as their Englishtranslation. Provide user with an option to look it up!
+
+dict={"pankha":"fan","dost":"friend","paani":"water","khidki":"window","darwaza":"door","madad":"help","pustak":"book"}
+
+word=input("Enter the Hindi word you want to look for:")
+
+print("The English word :",dict[word])
