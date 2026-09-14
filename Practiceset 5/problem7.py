ca9a49fe04af2aa15d52e158325c9e19af7f3313
Shruti-Dapakekar <shrutidapakekar@gmail.com>
2026-09-14 07:38:50 +0530

    problem 1 solved

--- Practiceset 5/problem7.py
+++ Practiceset 5/problem7.py
@@ -2,16 +2,16 @@
 dict ={}
 a1= input("Enter your name:")
 b1= input("Enter your favorite language:")
+dict.update({a1:b1})
 a2= input("Enter your name:")
 b2= input("Enter your favorite language:")
+dict.update({a2:b2})
 a3= input("Enter your name:")
 b3= input("Enter your favorite language:")
+dict.update({a3:b3})
 a4= input("Enter your name:")
 b4= input("Enter your favorite language:")
-dict[a1]=b1
-dict[a2]=b2
-dict[a3]=b3
-dict[a4]=b4 
+dict.update({a4:b4})
 print(dict)
 # If the names of 2 friends are same; then the value of the first friend will be replaced by the value of the second friend. 
 # The dictionary will contain only one entry for that name with the latest value.
