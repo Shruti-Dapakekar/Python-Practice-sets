e76e8e708a72c71b139c95d7249055e92712cef5
Shruti-Dapakekar <shrutidapakekar@gmail.com>
2026-09-14 08:51:31 +0530

    problem 7 solved

--- Practiceset 6/problem7.py
+++ Practiceset 6/problem7.py
@@ -2,7 +2,7 @@
 # Write a program to find out whether a given post is talking about “Shruti” or not.
 post = input("Enter the post: ")
 
-if "Shruti" in post:
+if "Shruti".lower() in post.lower():
     print("Yes, the post is talking about Shruti.")
 else:
     print("No, the post is not talking about Shruti.")
