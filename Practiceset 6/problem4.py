a7dd25520d21cc711ed01c0207415c2ca7025b94
Shruti-Dapakekar <shrutidapakekar@gmail.com>
2026-09-14 08:42:09 +0530

    problem 4 solved

---
+++ Practiceset 6/problem4.py
@@ -0,0 +1,6 @@
+# Write a program to find whether a given username contains less than 10 characters or not.
+username = input("Enetr your nusername:")
+if len(username)<10:
+    print("Username is valid.")
+else:  
+    print("Username is not valid.")
