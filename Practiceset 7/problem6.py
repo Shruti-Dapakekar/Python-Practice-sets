386fe9c9b1406086ffb0488395577e5f552ff1de
Shruti-Dapakekar <shrutidapakekar@gmail.com>
2026-09-16 22:05:29 +0530

    Solved problem 6

---
+++ Practiceset 7/problem6.py
@@ -0,0 +1,6 @@
+# Write a program to calculate the factorial of a given number using for loop.
+a = int(input("Enter a number:"))
+factorial = 1
+for i in range(1,a+1):
+    factorial = factorial * i
+print(f"The factorial of {a} is  {factorial}")
