dff8ffdd6e37abcba1b78902a8af29526027d495
Shruti-Dapakekar <shrutidapakekar@gmail.com>
2026-09-17 12:35:04 +0530

    problrm 6 solved

---
+++ Practiceset 8/problem6.py
@@ -0,0 +1,7 @@
+# Write a python function which converts inches to cms.
+def inch_to_cm():  #write inch in paraentheses
+    cm = inch * 2.54
+    print(f"{cm}")
+# inch_to_cm(5)
+inch = int(input("Enter inch: "))
+inch_to_cm()
