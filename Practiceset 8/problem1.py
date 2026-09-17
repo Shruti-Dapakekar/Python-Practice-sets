850c8dec4c4cd6ec63c39512c307f58dfc7299a1
Shruti-Dapakekar <shrutidapakekar@gmail.com>
2026-09-17 11:44:06 +0530

    problrm 1 solved

---
+++ Practiceset 8/problem1.py
@@ -0,0 +1,17 @@
+# Write a program using functions to find greatest of three numbers.
+def greatest():
+    if a>b and a>c:
+        print(f"{a} is Greater number")
+    elif b>a and b>c:
+        print(f"{b} is Greater number")
+    elif c>a and c>b:
+        print(f"{c} is Greater number")
+a = int(input("Enter 1st number:"))
+b = int(input("Enter 2nd number:"))
+c = int(input("Enter 3rd number:"))
+greatest()
+a = int(input("Enter 1st number:"))
+b = int(input("Enter 2nd number:"))
+c = int(input("Enter 3rd number:"))
+greatest()
+
