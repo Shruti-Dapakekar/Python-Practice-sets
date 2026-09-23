d323c5211a34b498ab600bece4ca1cf6b385b880
Shruti-Dapakekar <shrutidapakekar@gmail.com>
2026-09-23 13:48:24 +0530

    Problem 4 solved

---
+++ Practiceset_10/problem4.py
@@ -0,0 +1,19 @@
+# Add a static method in problem 2, to greet the user with hello.
+class Calculator:
+    def __init__(self,n):
+        self.n = n
+    def square(self):
+        print(f"The Square is {self.n*self.n}")
+    def cube(self):
+        print(f"The Cube is {self.n*self.n*self.n}")
+    def sq_root(self):
+        print(f"The Sq_Root is {self.n**1/2}")
+    @staticmethod
+    def greet():
+        print("Hello User")
+
+a= Calculator(4)
+a.greet()
+a.square()
+a.cube()
+a.sq_root()
