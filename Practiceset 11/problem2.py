bb26bc158707a24e031382acbc74167834396325
Shruti-Dapakekar <shrutidapakekar@gmail.com>
2026-09-25 15:59:06 +0530

    Problem 2 solved

---
+++ Practiceset 11/problem2.py
@@ -0,0 +1,15 @@
+# Create a class ‘Petsʼ from a class ‘Animalsʼ and further create a class ‘Dogʼ from ‘Petsʼ. Add a method ‘barkʼ to class ‘Dogʼ.
+class Animals:
+    pass
+    
+class Pets(Animals):
+    pass
+
+class Dogs(Pets):
+
+    @staticmethod
+    def bark():
+        print(f"Bow Bow!!")
+
+d= Dogs()
+d.bark()
